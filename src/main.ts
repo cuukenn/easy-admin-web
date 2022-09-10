bed59cd2f15f9116d44c3de0886a20ab769eca63
changgg <891842749@qq.com>
2022-09-10 13:47:32 +0800

    perf: add cdn dependency cache

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
 import '@/assets/css/main.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ElementPlus from 'element-plus'
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { viteMockServe } from 'vite-plugin-mock'
+import importToCDN from 'vite-plugin-cdn-import'
 
 //方法一
 export default defineConfig({
@@ -11,11 +12,46 @@ export default defineConfig({
       supportTs: true,
       mockPath: './src/mock',
     }),
+    importToCDN({
+      modules: [
+        {
+          name: 'vue',
+          var: 'Vue',
+          path: 'https://cdn.jsdelivr.net/vue@3.2.13',
+        },
+        {
+          name: 'element-plus',
+          var: 'ElementPlus',
+          path: `https://cdn.jsdelivr.net/npm/element-plus@2.2.14`,
+          css: 'https://cdn.jsdelivr.net/npm/element-plus@2.2.14/dist/index.css',
+        },
+        {
+          name: 'mockjs',
+          var: 'mockjs',
+          path: `https://cdn.jsdelivr.net/npm/mockjs@1.1.0`,
+        },
+        {
+          name: 'echarts',
+          var: 'echarts',
+          path: `https://cdn.jsdelivr.net/npm/echarts@5.3.3`,
+        },
+      ],
+    }),
   ],
   base: '/x-admin-web',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: '[ext]/[name]-[hash].[ext]',
+      },
     },
   },
 })
