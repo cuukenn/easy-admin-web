0445d6558f19afdaa9f05d360cdf8a9428c70db1
changgg <891842749@qq.com>
2022-09-10 13:31:56 +0800

    Revert "chore: 按需导入elemntplus"
    This reverts commit c235271816c5752dfa9cf54b3390e569531dddc1.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import '@/assets/css/main.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus from 'element-plus'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -12,4 +15,4 @@ const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component as any)
 }
-app.use(store).use(router).mount('#app')
+app.use(store).use(router).use(ElementPlus).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { viteMockServe } from 'vite-plugin-mock'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 //方法一
 export default defineConfig({
@@ -13,12 +10,6 @@ export default defineConfig({
     viteMockServe({
       supportTs: true,
       mockPath: './src/mock',
-    }),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
     }),
   ],
   base: '/x-admin-web',
